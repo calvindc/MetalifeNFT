eb372b387169aa50411930f02d3ce657220b5035
calvindc <466862683@qq.com>
2022-06-11 11:20:12 +0800

    init compiler

--- contracts/buildcontract.sh
+++ contracts/buildcontract.sh
@@ -9,16 +9,16 @@ abigen  --abi ./../abi/metaMaster/metaMaster.abi \
         --bin ./../abi/metaMaster/metaMaster.bin \
         --type metaMaster \
         --pkg metamaster \
-        --out ./../abi/metaMaster.go
+        --out ./../abi/metaMaster/metaMaster.go
 
 abigen  --abi ./../abi/saleAuction/saleAuction.abi \
         --bin ./../abi/saleAuction/saleAuction.bin \
         --type saleAuction \
         --pkg saleauction \
-        --out ./../abi//saleAuction.go
+        --out ./../abi/saleAuction/saleAuction.go
 abigen  --abi ./../abi/salePlain/salePlain.abi \
         --bin ./../abi/salePlain/salePlain.bin \
         --type salePlain \
         --pkg saleplain \
-        --out ./../abi//salePlain.go
+        --out ./../abi/salePlain/salePlain.go
 
